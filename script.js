1708f72c99a34e0a92614215530051493cb65896
Lameck Kipsang <lameck.ky@gmail.com>
2026-07-25 17:27:27 +0300

    add initialization and local storage

--- script.js
+++ script.js
@@ -5,3 +5,11 @@ const foodList = document.getElementById('food-list');
 const totalCaloriesDisplay = document.getElementById('total-calories');
 const resetBtn = document.getElementById('reset-btn');
 const simulateFetchBtn = document.getElementById('simulate-fetch-btn');
+const baseURL = "https://world.openfoodfacts.org";
+let foodItems = JSON.parse(localStorage.getItem('foodItems')) || [];
+function init() {
+    renderApp();
+}
+function saveToLocalStorage() {
+    localStorage.setItem('foodItems', JSON.stringify(foodItems));
+}
