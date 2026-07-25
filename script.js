ca621df4d5714d53cdb01c4e0dbf8bf5a1b2a939
Lameck Kipsang <lameck.ky@gmail.com>
2026-07-25 18:08:18 +0300

    add a function implementing API fetch using baseURL

--- script.js
+++ script.js
@@ -63,3 +63,46 @@ function resetCalories() {
         renderApp();
     }
 }
+function getFoodData() {
+    simulateFetchBtn.textContent = "Loading...";
+    simulateFetchBtn.disabled = true;
+
+    const barcodes = [
+        "3017624010701", //Nutella
+        "5449000000996", //Coca-Cola
+        "3274080005003"  //Evian Water
+    ];
+    //Picks a random barcode from the array to simulate scanning a product
+    const randomBarcode = barcodes[Math.floor(Math.random() * barcodes.length)];
+    // Combines the baseURL, endpoint path, random barcode, and file extension to build the full API URL
+    const url = baseURL + "/api/v2/product/" + randomBarcode + ".json";
+
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            if (data.product) {
+                let productName = data.product.product_name;
+                if (!productName) {
+                    productName = "Food Item";
+                }
+
+                let calories = data.product.nutriments['energy-kcal_100g'];
+                if (!calories) {
+                    calories = 200;
+                }
+
+                foodNameInput.value = productName;
+                foodCaloriesInput.value = calories;
+            } else {
+                alert("Product not found.");
+            }
+        })
+        .catch(error => {
+            console.error("Error fetching food data", error);
+            alert("Could not fetch data from Open Food Facts.");
+        })
+        .finally(() => {
+            simulateFetchBtn.textContent = "Fetch from API";
+            simulateFetchBtn.disabled = false;
+        });
+}
