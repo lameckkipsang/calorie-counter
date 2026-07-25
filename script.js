b38dcdafccf563546a7cbfd19f88f4213ab8638e
Lameck Kipsang <lameck.ky@gmail.com>
2026-07-25 16:43:21 +0300

    assign variable in js to its id in index.html

---
+++ script.js
@@ -0,0 +1,7 @@
+const foodForm = document.getElementById('food-form');
+const foodNameInput = document.getElementById('food-name');
+const foodCaloriesInput = document.getElementById('food-calories');
+const foodList = document.getElementById('food-list');
+const totalCaloriesDisplay = document.getElementById('total-calories');
+const resetBtn = document.getElementById('reset-btn');
+const simulateFetchBtn = document.getElementById('simulate-fetch-btn');
